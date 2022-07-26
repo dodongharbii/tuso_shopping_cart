cfde419a8645d7fda093917defbde311f1061a6c
harbii <vonnehrvy98@gmail.com>
2022-07-26 11:06:21 +0800

    imported second page from homepage

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:provider/provider.dart';
-//import 'package:provider_test/second_page.dart';
+import 'second_page.dart';
 
 import 'data_class.dart';
 
@@ -13,97 +13,90 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFFfefcff),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children:  [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 40),
-            child: Row(
-              children: [
-                Consumer<DataClass>(builder: (context, data, child){
-                  return Text('${data.x}', style: TextStyle(
-                    fontSize: 20,fontWeight: FontWeight.bold
-                  ),);
-                }),
-                Spacer(),
-                Text("Total", style: TextStyle(fontWeight: FontWeight.bold,
-                fontSize: 40),)
-              ],
-            ),
+      body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 40),
+          child: Row(
+            children: [
+              Consumer<DataClass>(builder: (context, data, child) {
+                return Text(
+                  '${data.x}',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                );
+              }),
+              Spacer(),
+              Text(
+                "Total",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40),
+              )
+            ],
           ),
-          SizedBox(height: 100,),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 40),
-            child: Row(
-              children: [
-
-                GestureDetector(child: Container(
+        ),
+        SizedBox(
+          height: 100,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 40),
+          child: Row(
+            children: [
+              GestureDetector(
+                child: Container(
                   width: 60,
                   height: 60,
                   child: Icon(Icons.add),
                   decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(10),
-
-                      border: Border.all(
-                          color: Color(0xFF716f72),
-                          width: 1
-                      )
-                  ),
+                      border: Border.all(color: Color(0xFF716f72), width: 1)),
                 ),
-                onTap: (){
-                  if(Provider.of<DataClass>(context, listen: false).x>=5){
+                onTap: () {
+                  if (Provider.of<DataClass>(context, listen: false).x >= 5) {
                     Get.snackbar("Item", "Can not more than this",
                         backgroundColor: Colors.black,
                         colorText: Colors.white,
                         titleText: Text(
                           "Item",
-                          style: TextStyle(
-                              fontSize: 40,
-                              color: Colors.white
-                          ),
+                          style: TextStyle(fontSize: 40, color: Colors.white),
                         ),
                         messageText: Text(
                           "Can not be more than this",
-                          style: TextStyle(
-                              fontSize: 20,
-                              color: Colors.white
-                          ),
-                        )
-                    );
-                  }else{
+                          style: TextStyle(fontSize: 20, color: Colors.white),
+                        ));
+                  } else {
                     Provider.of<DataClass>(context, listen: false).incrementX();
                   }
-                },),
-                Spacer(),
-                Container(
-                  height: 60,
-                  width: 200,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-
-                      color: Colors.black
+                },
+              ),
+              Spacer(),
+              Container(
+                height: 60,
+                width: 200,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.black),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: Row(
+                    children: [
+                      GestureDetector(
+                          onTap: () {
+                            Get.to(() => SecondPage(),
+                                transition: Transition.upToDown,
+                                duration: Duration(seconds: 1));
+                          },
+                          child: Text(
+                            "Next Page",
+                            style: TextStyle(fontSize: 20, color: Colors.white),
+                          )),
+                      Spacer(),
+                      Icon(Icons.skip_next, color: Colors.white)
+                    ],
                   ),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 10),
-                    child: Row(
-
-                      children: [
-                        GestureDetector(
-                            onTap: (){
-                              //Get.to(()=>SecondPage(), transition: Transition.upToDown, duration: Duration(seconds: 1));
-                            },
-                            child: Text("Next Page", style: TextStyle(fontSize: 20, color: Colors.white),)),
-                        Spacer(),
-                        Icon(Icons.skip_next, color:Colors.white)
-                      ],
-                    ),
-                  ),
-                )
-              ],
-            ),
-          )
-        ]
-      ),
+                ),
+              )
+            ],
+          ),
+        )
+      ]),
     );
   }
 }
